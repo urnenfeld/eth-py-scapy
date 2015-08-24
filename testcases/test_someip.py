4cdcbd5b2927cf79984bc6e14366009c55b3f028
jamores <biomol@gmail.com>
2015-08-24 23:10:10 +0200

    first SOME/IP-SD test cases

--- testcases/test_someip.py
+++ testcases/test_someip.py
@@ -6,6 +6,9 @@ from test_base import baseTest
 
 from scapy.all import *
 from scapy_ext import scapy_helper as helper
+from scapy_ext import *
+
+ETH_IFACE = "eth1.10"
 
 class test_somepip(baseTest):
 
@@ -31,8 +34,33 @@ class test_somepip(baseTest):
         sip.retcode = 0x0
 
         # send message
-        p = Ether()/IP()/TCP()/sip
-        ans = helper.srp1(p,iface="")
+        p = Ether()/IP(dst="10.0.0.11")/TCP(sport=30490,dport=30490)/sip
+        ans = helper.sendp(p,iface="eth2.10")
 
         # log test results
-        self.addTestCase("ping to google.com",error=err_msg)
+        self.addTestCase("send SOMEIP Magic cookie",error=err_msg)
+    
+    # Test 01 : SOME/IP-SD : Find service
+    def test_01(self):
+        """SOME/IP-SD packet."""
+        err_msg = ""
+
+        # build SOME/IP-SD packet
+        sip = helper.newSOMEIP()
+        sip.msg_id.srv_id = 0xffff
+        sip.msg_id.sub_id = 0x1
+        sip.msg_id.evt_id = 0x100
+        sip.req_id.client_id = 0x00
+        sip.req_id.session_id = 0x01
+        sip.type = SOMEIP.TYPE_NOTIFICATION
+        
+        sd = helper.newSD()
+        sd.flags = 0xff
+        sd.entry_array = [SDEntry_Service(type=0,srv_id=0x1111,inst_id=0x2222,major_ver=0x01,ttl=0x02)]
+        
+        # SEND MESSAGE
+        p = Ether()/IP(dst="10.0.0.11")/UDP(sport=30490,dport=30490)/sip/sd
+        ans = helper.srp1(p,iface="eth2.10")
+        
+        # log test results
+        self.addTestCase("send SOME/IP-SD : find service",error=err_msg)
